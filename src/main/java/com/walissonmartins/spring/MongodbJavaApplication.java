fa08e6792a97ad7abeea9050f314cc250dd714ea
walisson-martins <walisson.martinsti@gmail.com>
2021-05-14 10:52:25 -0300

    Primeiro projeto criado

---
+++ src/main/java/com/walissonmartins/spring/MongodbJavaApplication.java
@@ -0,0 +1,13 @@
+package com.walissonmartins.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walissonmartins/spring/MongodbJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walissonmartins.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
